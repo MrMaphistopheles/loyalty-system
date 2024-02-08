40199788c3ce1cac872d46594595e7cd9242c92b
MrMaphistopheles <basylmarchak@icloud.com>
2024-02-08 21:16:25 +0200

    pwa on

--- next.config.js
+++ next.config.js
@@ -20,10 +20,10 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 });
 
 const config = {
-  images:{
-    domains: ["storage.googleapis.com"]
-  }
+  images: {
+    domains: ["storage.googleapis.com"],
+  },
 };
 
-//export default withPWA(config);
-export default config;
+export default withPWA(config);
+//export default config;
