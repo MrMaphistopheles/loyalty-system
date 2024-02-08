7270077b3e27f252e6ff2033df9b11b39699dbcb
MrMaphistopheles <basylmarchak@icloud.com>
2024-02-08 17:06:44 +0200

    metadata update

--- src/app/[company]/layout.tsx
+++ src/app/[company]/layout.tsx
@@ -20,42 +20,56 @@ export async function generateMetadata(
         path_key: params.company,
       },
       select: {
+        userId: true,
         icons: true,
       },
     });
 
-    return pathKey;
+    const companyName = await db.user.findUnique({
+      where: {
+        id: pathKey?.userId,
+      },
+      select: {
+        name: true,
+      },
+    });
+
+    return { ...pathKey, ...companyName };
   }
 
   const key = await getUserKeyPath();
 
-  const maifest = {
-    name: "Bonus App",
-    short_name: "Bonus App",
-    icons: [
-      {
-        src: key?.icons[0]?.size_96,
-        sizes: "96x96",
-      },
-      {
-        src: key?.icons[0]?.size_192,
-        sizes: "192x192",
-      },
-      {
-        src: key?.icons[0]?.size_512,
-        sizes: "512x512",
-      },
-    ],
-    theme_color: "#FFFFFF",
-    background_color: "#FFFFFF",
-    start_url: `http://localhost:3000/${params.company}`,
-    display: "standalone",
-    orientation: "portrait",
-  };
+  let stringifide = "";
 
-  const stringifide = JSON.stringify(maifest);
+  if (key.icons && key.icons[0]) {
+    const manifest = {
+      name: key.name,
+      short_name: key.name,
+      icons: [
+        {
+          src: key?.icons[0]?.size_96,
+          sizes: "96x96",
+        },
+        {
+          src: key?.icons[0]?.size_192,
+          sizes: "192x192",
+        },
+        {
+          src: key?.icons[0]?.size_512,
+          sizes: "512x512",
+        },
+      ],
+      theme_color: "#FFFFFF",
+      background_color: "#FFFFFF",
+      start_url: `${process.env.NEXT_PUBLIC_CALLBACK_URL_FOR_USER}/${params.company}`,
+      display: "standalone",
+      orientation: "portrait",
+    };
+    stringifide = JSON.stringify(manifest);
+  }
 
   return {
+    title: key.name,
     manifest:
       "data:application/json;charset=utf-8," + encodeURIComponent(stringifide),
   };
