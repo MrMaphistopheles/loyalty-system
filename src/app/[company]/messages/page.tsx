3eb316c023072a42877e396e60c19ba52ee9b67e
MrMaphistopheles <basylmarchak@icloud.com>
2024-02-10 10:38:24 +0200

    bug fixed

--- src/app/[company]/messages/page.tsx
+++ src/app/[company]/messages/page.tsx
@@ -1,12 +1,10 @@
-
-
 import Authoraised from "@/app/_components/app/Authoraised";
 import Message from "./_comp/Message";
 
 export default function Page({ params }: { params: { company: string } }) {
   return (
     <Authoraised role="USER" company={params.company}>
-      <Message company={params.company}/>
+      <Message company={params.company} />
     </Authoraised>
   );
 }
--- src/app/api/fondy-response/route.ts
+++ src/app/api/fondy-response/route.ts
@@ -6,5 +6,5 @@ export async function POST(req: NextRequest) {
   const body = await req.json();
   console.log(body);
 
-  return Response.json({ body });
+  return Response.json({ body, msg: "working" });
 }
