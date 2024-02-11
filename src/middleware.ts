aa916215024da42d202e0fb1c8a0a267e6da8611
MrMaphistopheles <basylmarchak@icloud.com>
2024-02-11 23:20:26 +0200

    s

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,10 @@ export function middleware(req: NextRequest) {
 
   res.headers.append("Access-Control-Allow-Credentials", "true");
   res.headers.append("Access-Control-Allow-Origin", "https://pay.fondy.eu");
-  res.headers.append("Access-Control-Allow-Methods", "POST");
+  res.headers.append(
+    "Access-Control-Allow-Methods",
+    "GET,DELETE,PATCH,POST,PUT",
+  );
   res.headers.append(
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
