e609f532447b61e066f0193801dafee5bd683a5b
MrMaphistopheles <basylmarchak@icloud.com>
2024-02-11 22:55:31 +0200

    await

--- src/app/api/fondy-response/route.ts
+++ src/app/api/fondy-response/route.ts
@@ -2,5 +2,5 @@ import { db } from "@/server/db";
 import { getServerAuthSession } from "../../../server/auth";
 
 export async function POST(req: Request) {
-  return Response.json(req);
+  return await Response.json(req);
 }
