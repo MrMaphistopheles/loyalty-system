ebaf9cc916e800c7b548b16688a0d32d8613cfc8
MrMaphistopheles <basylmarchak@icloud.com>
2024-02-11 22:52:28 +0200

    f

--- src/app/api/fondy-response/route.ts
+++ src/app/api/fondy-response/route.ts
@@ -2,9 +2,5 @@ import { db } from "@/server/db";
 import { getServerAuthSession } from "../../../server/auth";
 
 export async function POST(req: Request) {
-  const body = await req.json();
-
-  console.log(body);
-
-  return Response.redirect("https://lite-theta.vercel.app");
+  return Response.json(req);
 }
